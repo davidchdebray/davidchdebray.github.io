c895339575f280b2ffc0a22c8189852cb7ba5840
didav64 <didav64@hotmail.com>
2020-11-10 17:24:30 +0100

    up

--- export_html_pdf.sh
+++ export_html_pdf.sh
@@ -2,7 +2,7 @@
 
 cp cv.md Backups/"$(date +%Y-%m-%d_cv)".md
 
-pandoc cv.md -s -c styleDark.css -A footer.html --toc -o cv.html
+pandoc cv.md -s -c styleDark.css -A footer.html --toc -o DavidDebrayCV.html
 #pandoc cv.md -s -c styleLightDark.css -A footer.html --toc -o cv.html
-pandoc cv.md --template=template.tex --pdf-engine=xelatex -o cv.pdf
+pandoc cv.md --template=template.tex --pdf-engine=xelatex -o DavidDebrayCV.pdf
 
