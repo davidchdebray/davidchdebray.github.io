c87556616db33821dc294cf87440e06a4886b4c8
David Debray <david.debray@protonmail.com>
2021-10-12 17:25:41 +0200

    Update CV.pdf

--- BuildCV.sh
+++ BuildCV.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# cp cv.md Backups/"$(date +%Y-%m-%d_cv)".md
 
-pandoc cv.md -s -c styleDark.css -A footer.html --toc -o DavidDebrayCV.html && pandoc cv.md --template=template.tex --pdf-engine=xelatex -o DavidDebrayCV.pdf
-#pandoc cv.md -s -c styleLightDark.css -A footer.html --toc -o cv.html
+pandoc cv.md -s -c styleDark.css -A footer.html --toc -o DavidDebrayCV.html \
+&& pandoc cv.md --template=template.tex --pdf-engine=xelatex -o DavidDebrayCV.pdf
